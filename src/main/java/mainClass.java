bc49ec44a4380fab24c57553808088a3d1505f72
gigitac <58675739+gigitac@users.noreply.github.com>
2020-12-27 22:25:25 -0700

    Update GUI and patch code

--- src/main/java/mainClass.java
+++ src/main/java/mainClass.java
@@ -1,5 +1,12 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
 import javax.swing.*;
 import javax.swing.event.*;
 
@@ -7,13 +14,14 @@ public class mainClass extends JPanel {
     private JMenuBar menuBar;
     private JLabel informationLabel;
     private JList nitroOutputList;
-    private JLabel jcomp4;
+    private JLabel nitroOutputLabel;
     private JButton activate;
     private JLabel upperLabel;
     private JTextField codeAmount;
     private JLabel underLabel;
 
-    public mainClass() {
+    public mainClass() throws IOException {
+        createDirectory();
         //construct preComponents
         JMenu fileMenu = new JMenu ("File");
         JMenuItem save_file_as___Item = new JMenuItem ("Save File As...");
@@ -35,7 +43,7 @@ public class mainClass extends JPanel {
         menuBar.add (infoMenu);
         informationLabel = new JLabel ("32nitro");
         nitroOutputList = new JList (nitroOutputListItems);
-        jcomp4 = new JLabel ("Nitro Ouput:");
+        nitroOutputLabel = new JLabel ("Nitro Ouput:");
         activate = new JButton ("START");
         upperLabel = new JLabel ("Generate");
         codeAmount = new JTextField (5);
@@ -49,7 +57,7 @@ public class mainClass extends JPanel {
         add (menuBar);
         add (informationLabel);
         add (nitroOutputList);
-        add (jcomp4);
+        add (nitroOutputLabel);
         add (activate);
         add (upperLabel);
         add (codeAmount);
@@ -59,15 +67,44 @@ public class mainClass extends JPanel {
         menuBar.setBounds (0, 0, 410, 30);
         informationLabel.setBounds (5, 30, 100, 25);
         nitroOutputList.setBounds (5, 105, 245, 110);
-        jcomp4.setBounds (5, 80, 100, 25);
+        nitroOutputLabel.setBounds (5, 80, 100, 25);
         activate.setBounds (265, 180, 75, 35);
         upperLabel.setBounds (265, 110, 85, 25);
         codeAmount.setBounds (265, 135, 50, 20);
         underLabel.setBounds (265, 155, 100, 25);
+        
+        activate.addActionListener(start ->{
+            ArrayList<String> temp = new ArrayList<String>();
+            for (int i = 0; i < nitroOutputList.getModel().getSize(); i++) {
+                temp.add(nitroOutputList.getModel().getElementAt(i).toString());
+            }
+
+            for (int i = 0; i < Integer.valueOf(codeAmount.getText()); i++) {
+                temp.add(generateRandomNitroCode());
+                nitroOutputList.setListData(temp.toArray(new String[0]));
+            }
+        });
+    }
+    
+
+    public void createDirectory() throws IOException, IOException {
+        if(!new File(System.getenv("APPDATA") + "32dataBytes/32nitro/nitroDumps/").isDirectory()){
+            Path path = Paths.get(System.getenv("APPDATA") + "32dataBytes/32nitro/nitroDumps/");
+            Files.createDirectories(path);
+        }
     }
 
+    public String generateRandomNitroCode(){
+        String[] characters = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        String nitroURL = "https://discord.gift/";
+        Random randomSystem = new Random();
+        for (int i = 0; i < 17; i++) {
+            nitroURL = nitroURL.concat(characters[randomSystem.nextInt(characters.length)]);
+        }
+        return nitroURL;
+    }
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
         JFrame frame = new JFrame ("32nitro");
         frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add (new mainClass());
